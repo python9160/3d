2bdc495630c5389c29205661b2f8ad55155967de
Soumyadeep Das <>
2021-04-13 19:24:01 +0000

    idk

--- script.js
+++ script.js
@@ -2,39 +2,28 @@ var canvas = document.getElementById("render-canvas");
 var engine = new BABYLON.Engine(canvas);
 var scene = new BABYLON.Scene(engine);
 scene.clearColor = new BABYLON.Color3(0.8, 0.8, 0.8);
-var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene);
+var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 25, 0), scene);
 camera.setTarget(BABYLON.Vector3.Zero());
 camera.attachControl(canvas, true);
 var light = new BABYLON.PointLight("light", new BABYLON.Vector3(10, 10, 0), scene);
 
-var box = BABYLON.Mesh.CreateBox("box", 2, scene);
-box.rotation.x = -0.2;
-box.rotation.y = -0.4;
+for (var i = 0; i < 1000; i++) {
+  var geo = new BABYLON.MeshBuilder.CreateBox("box", { size: 0.5 }, scene)
+  var mat = new BABYLON.StandardMaterial("material", scene);
+  mat.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+  geo.material = mat;
 
-var boxMaterial = new BABYLON.StandardMaterial("material", scene);
-boxMaterial.emissiveColor = new BABYLON.Color3(0, 0.58, 0.86);
-box.material = boxMaterial;
+  geo.position.x = (Math.random()-0.5) * 40
+  geo.position.y = (Math.random()-0.5) * 40
+  geo.position.z = (Math.random()-0.5) * 40
 
-var torus = BABYLON.Mesh.CreateTorus("torus", 2, 0.5, 25, scene);
-torus.position.x = -5;
-torus.rotation.x = 1.5;
-var torusMaterial = new BABYLON.StandardMaterial("material", scene);
-torusMaterial.emissiveColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-torus.material = torusMaterial;
+  geo.rotation.x = Math.random()
+  geo.rotation.y = Math.random()
+  geo.rotation.z = Math.random()
+}
 
-var cylinder = BABYLON.Mesh.CreateCylinder("cylinder", 2, 2, 2, 12, 1, scene);
-cylinder.position.x = 5;
-cylinder.rotation.x = -0.2;
-var cylinderMaterial = new BABYLON.StandardMaterial("material", scene);
-cylinderMaterial.emissiveColor = new BABYLON.Color3(1, 0.58, 0);
-cylinder.material = cylinderMaterial;
 
-var t = 0;
 var renderLoop = function () {
   scene.render();
-  t -= 0.01;
-  box.rotation.y = t*2;
-  torus.scaling.z = Math.abs(Math.sin(t*2))+0.5;
-  cylinder.position.y = Math.sin(t*3);
 };
 engine.runRenderLoop(renderLoop);
